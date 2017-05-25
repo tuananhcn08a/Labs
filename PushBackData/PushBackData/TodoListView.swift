81e7008578be63d6259e9bbe5fc424c9073dadd3
Ragnar <anhdt4@gmail.com>
2017-05-25 10:36:33 +0700

    Add stater source

---
+++ PushBackData/PushBackData/TodoListView.swift
@@ -0,0 +1,56 @@
+//
+//  TodoListView.swift
+//  PushBackData
+//
+//  Created by Ragnar on 5/25/17.
+//  Copyright © 2017 Ragnar. All rights reserved.
+//
+
+import UIKit
+
+class TodoListView: UIViewController,  UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tbTodoList: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func btnAddItemClicked(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "addItemSegue" {
+            
+        }
+    }
+
+    // MARK: --- UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // Table View Cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath) as UITableViewCell
+        
+        return cell
+    }
+}
